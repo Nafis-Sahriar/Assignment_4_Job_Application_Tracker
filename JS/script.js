b1fe20c535077ab891fdd4bc23c000faa1b520d1
Nafis-Sahriar <nafis.apid@gmail.com>
2026-02-22 16:00:17 +0600

    Reject Section done partially

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,7 @@ function getById(id)
 let interviewList =[];
 let rejectList = [];
 renderInterview();
+renderRejected();
 
 
 const buttonAll = document.getElementById('btn-all');
@@ -24,40 +25,38 @@ const buttonRejectedPage = document.getElementById('btn-rejected-page');
 
 function btnAnimation(id)
 {
-    const all_card_container = document.getElementById('all-card-container');
-    const intervew_card_container = document.getElementById('interview-section');
+    const all_card_container = getById('all-card-container');
+    const intervew_card_container = getById('interview-section');
+    const rejectCardContainer = getById('rejected-section');
+
+    buttonAll.classList.remove('bg-blue-500', 'text-white');
+    buttonInterviewPage.classList.remove('bg-blue-500', 'text-white');
+    buttonRejectedPage.classList.remove('bg-blue-500', 'text-white');
+
+    const clickedBtn = getById(id);
+
+    clickedBtn.classList.add('bg-blue-500','text-white');
 
     if(id=='btn-all')
     {
-        buttonInterviewPage.classList.remove('bg-blue-500', 'text-white');
-        buttonRejectedPage.classList.remove('bg-blue-500','text-white');
-        buttonAll.classList.add('bg-blue-500', 'text-white');
-
         all_card_container.classList.remove('hidden');
         intervew_card_container.classList.add('hidden');
-
-
-
-
+        rejectCardContainer.classList.add('hidden');
     }
 
     else if(id=='btn-interview-page')
     {
-        buttonInterviewPage.classList.add('bg-blue-500', 'text-white');
-        buttonRejectedPage.classList.remove('bg-blue-500','text-white');
-        buttonAll.classList.remove('bg-blue-500','text-white');
-
-
         all_card_container.classList.add('hidden');
         intervew_card_container.classList.remove('hidden');
-
+        rejectCardContainer.classList.add('hidden');
     }
     else if(id=='btn-rejected-page')
     { 
-        buttonInterviewPage.classList.remove('bg-blue-500', 'text-white');
-        buttonRejectedPage.classList.add('bg-blue-500', 'text-white');
-        buttonAll.classList.remove('bg-blue-500');
 
+        all_card_container.classList.add('hidden');
+        intervew_card_container.classList.add('hidden');
+        rejectCardContainer.classList.remove('hidden');
+        
     }
 }
 
@@ -96,6 +95,12 @@ function countInterview()
     
 }
 
+function countRejected()
+{
+    const rejectedCount = rejectList.length;
+    getById('rejected_count').innerText = rejectedCount;
+}
+
 
 
 // Delete Button Functionality
@@ -117,7 +122,8 @@ const mainContainer = document.querySelector("main");
 
 // console.log(mainContainer);
 
-mainContainer.addEventListener('click', function(event){
+mainContainer.addEventListener('click', function(event)
+{
 
     // console.log(event.target);
 
@@ -160,19 +166,61 @@ mainContainer.addEventListener('click', function(event){
             
             const badge = baap.querySelector('.status-badge');
             badge.innerText='Interview';
-            badge.classList.add('bg-green-300', 'text-green-800');
+            badge.classList.add('bg-green-300', 'text-green-800', 'border-4', 'border-green-700', 'font-bold', 'text-md');
+            badge.classList.remove('bg-red-300', 'text-red-800','border-2','border-red-500');
             
             
             
-
             countInterview();
-
             renderInterview();
-
-          
         }
         
         
+   }
+
+   if(event.target.classList.contains('btn-rejected'))
+   {
+    const baap = event.target.parentNode.parentNode;
+
+    const companyName = baap.querySelector('.company-name').innerText;
+    // console.log(companyName);
+
+    const jobRole = baap.querySelector('.job-role').innerText;
+    // console.log(jobRole);
+
+    const jobSalary = baap.querySelector('.job-salary').innerText;
+    // console.log(jobSalary);
+    const statusBadge = baap.querySelector('.status-badge').innerText;
+    // console.log(statusBadge);
+
+    const jobTitle = baap.querySelector('.job-title').innerText;
+    // console.log(jobTitle); 
+
+    let cardInfo = 
+    {
+        companyName,
+        jobRole,
+        jobSalary,
+        statusBadge,
+        jobTitle
+    };
+
+    const rejectedExist= rejectList.find(item =>item.companyName===cardInfo.companyName);
+
+    if(!rejectedExist)
+    {
+        rejectList.push(cardInfo);
+
+        const badge = baap.querySelector('.status-badge');
+        badge.innerText='Rejected';
+        badge.classList.remove('bg-green-300','text-green-800');
+        badge.classList.add('bg-red-300', 'text-red-800','border-2','border-red-500' );
+
+
+
+        renderRejected();
+        countRejected();
+    }
    }
 
 })
@@ -216,11 +264,11 @@ function renderInterview()
             </p>
 
             <p class="job-salary text-gray-500 mt-3">
-                ${interview.jobSalary.innerText}
+                ${interview.jobSalary}
             </p>
 
             <div class="status-div">
-                <p class="status-badge mt-4 inline-block px-4 py-2 text-sm font-bold border border-[3px] border-green-600 bg-green-300 text-green-800 rounded-md">
+                <p class="status-badge mt-4 inline-block px-4 py-2 text-sm font-bold  border-4 border-green-600 bg-green-300 text-green-800 rounded-md">
                     Interview
                 </p>
             </div>
@@ -247,6 +295,63 @@ function renderInterview()
         container.appendChild(div);
     }
 }
+
+function renderRejected()
+{
+    const container = getById('rejected-card-container');
+    const noRejected = getById('no-rejected');
+
+    container.innerHTML = '';
+
+    if(rejectList.length === 0)
+    {
+        noRejected.classList.remove('hidden');
+        return;
+    }
+
+    noRejected.classList.add('hidden');
+
+    for(let rejected of rejectList)
+    {
+        let div = document.createElement('div');
+
+        div.innerHTML = `
+        <div class="bg-white rounded-xl border border-gray-200 p-6 relative mt-5 mb-5">
+
+            <button class="delete absolute top-4 right-4 p-2 rounded-full border border-gray-200 hover:bg-red-300">
+                <img src="./Resources/Trash.png" alt="">
+            </button>
+
+            <h2 class="company-name text-xl font-semibold text-blue-900">
+                ${rejected.companyName}
+            </h2>
+
+            <p class="job-role text-gray-600 mt-1">
+                ${rejected.jobRole}
+            </p>
+
+            <p class="job-salary text-gray-500 mt-3">
+                ${rejected.jobSalary}
+            </p>
+
+            <div class="status-div">
+                <p class="status-badge mt-4 inline-block px-4 py-2 text-sm font-bold border border-[3px] border-red-600 bg-red-300 text-red-800 rounded-md">
+                    Rejected
+                </p>
+            </div>
+
+            <p class="job-title text-gray-600 mt-4">
+                ${rejected.jobTitle}
+            </p>
+
+        </div>
+        `;
+
+        container.appendChild(div);
+    }
+}
+
+
 
 
 
